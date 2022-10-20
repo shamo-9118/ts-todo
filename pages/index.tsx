9afd6fc3e446f6218f8c4d0d2f23d2100c714937
shamo-9118 <won9118391183@gmail.com>
2022-10-21 06:17:08 +0900

    gitとの練習

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,7 +47,7 @@ const Home: NextPage = () => {
           className="border border-black flex-shrink-0 px-2"
           onClick={add}
         >
-          追加
+          追加します
         </button>
       </div>
       <ul className="mt-4 space-y-2">
